0fd24fde4b3a97dbf657c765d27414daa09b6356
Krishna Mishra <mkrishna@n2rtechnologies.in>
2022-09-29 14:11:29 +0530

    safari bookings

--- safari/index.js
+++ safari/index.js
@@ -1,24 +1,37 @@
 const express = require("express");
+const createError = require('http-errors');
 const app = express();
 //const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-
+const dotenv = require("dotenv").config();
+require('./initDB')();
 const cors = require("cors");
-
-dotenv.config();
-
-// mongoose.connect(process.env.MONGO_URL)
-//     .then(() => console.log("DB Connection Successfull!"))
-//     .catch((err) => {
-//         console.log(err);
-//     });
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/list', (req, res) => res.send("In safari list"));
+const PriceRoute = require('./Routes/Price.route');
+app.use('/prices', PriceRoute);
 
-app.get('/', (req, res) => res.send("In safari info page"));
+//404 handler and pass to error handler
+app.use((req, res, next) => {
+  next(createError(404, 'Not found'));
+});
+
+//Error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message
+    }
+  });
+});
+
+app.get('/list', (req, res) => res.send("In hotel list"));
+
+app.get('/', (req, res) => res.send("In hotel info page"));
 
 
 app.listen(process.env.PORT || 5003, () => {
