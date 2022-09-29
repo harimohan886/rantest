4c856a362964735462cde863377511e48c24e9b4
Krishna Mishra <mkrishna@n2rtechnologies.in>
2022-09-29 13:35:30 +0530

    chambal bookings

---
+++ chambal/Controllers/Price.Controller.js
@@ -0,0 +1,111 @@
+const createError = require('http-errors');
+const mongoose = require('mongoose');
+
+const Price = require('../Models/Price.model');
+
+module.exports = {
+  getAllPrices: async (req, res, next) => {
+  	console.log(req.body);
+    try {
+      const results = await Price.find({}, { __v: 0 });
+      // const results = await Price.find({}, { name: 1, price: 1, _id: 0 });
+      // const results = await Price.find({ price: 699 }, {});
+      res.send(results);
+    } catch (error) {
+      console.log(error.message);
+    }
+  },
+
+  createNewPrice: async (req, res, next) => {
+  	console.log(req.body);
+    try {
+      const product = new Price(req.body);
+      const result = await product.save();
+      res.send(result);
+    } catch (error) {
+      console.log(error.message);
+      if (error.name === 'ValidationError') {
+        next(createError(422, error.message));
+        return;
+      }
+      next(error);
+    }
+
+    /*Or:
+  If you want to use the Promise based approach*/
+    /*
+  const product = new Price({
+    name: req.body.name,
+    price: req.body.price
+  });
+  product
+    .save()
+    .then(result => {
+      console.log(result);
+      res.send(result);
+    })
+    .catch(err => {
+      console.log(err.message);
+    }); 
+    */
+  },
+
+  findPriceById: async (req, res, next) => {
+    const id = req.params.id;
+    try {
+      const product = await Price.findById(id);
+      // const product = await Price.findOne({ _id: id });
+      if (!product) {
+        throw createError(404, 'Price does not exist.');
+      }
+      res.send(product);
+    } catch (error) {
+      console.log(error.message);
+      if (error instanceof mongoose.CastError) {
+        next(createError(400, 'Invalid Price id'));
+        return;
+      }
+      next(error);
+    }
+  },
+
+  updateAPrice: async (req, res, next) => {
+    try {
+      const id = req.params.id;
+      const updates = req.body;
+      const options = { new: true };
+
+      const result = await Price.findByIdAndUpdate(id, updates, options);
+      if (!result) {
+        throw createError(404, 'Price does not exist');
+      }
+      res.send(result);
+    } catch (error) {
+      console.log(error.message);
+      if (error instanceof mongoose.CastError) {
+        return next(createError(400, 'Invalid Price Id'));
+      }
+
+      next(error);
+    }
+  },
+
+  deleteAPrice: async (req, res, next) => {
+    const id = req.params.id;
+    try {
+      const result = await Price.findByIdAndDelete(id);
+      // console.log(result);
+      if (!result) {
+        throw createError(404, 'Price does not exist.');
+      }
+      res.send(result);
+    } catch (error) {
+      console.log(error.message);
+      if (error instanceof mongoose.CastError) {
+        next(createError(400, 'Invalid Price id'));
+        return;
+      }
+      next(error);
+    }
+  }
+};
--- chambal/index.js
+++ chambal/index.js
@@ -1,21 +1,33 @@
 const express = require("express");
+const createError = require('http-errors');
 const app = express();
 //const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-
+const dotenv = require("dotenv").config();
+require('./initDB')();
 const cors = require("cors");
-
-dotenv.config();
-
-// mongoose.connect(process.env.MONGO_URL)
-//     .then(() => console.log("DB Connection Successfull!"))
-//     .catch((err) => {
-//         console.log(err);
-//     });
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+const PriceRoute = require('./Routes/Price.route');
+app.use('/prices', PriceRoute);
+
+//404 handler and pass to error handler
+app.use((req, res, next) => {
+  next(createError(404, 'Not found'));
+});
+
+//Error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message
+    }
+  });
+});
 
 app.get('/list', (req, res) => res.send("In hotel list"));
 
