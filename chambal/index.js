b56fbfa4d3a0516b39864fdb85b04813b64aa3b1
arsh <mmustaqueem@n2rtechnologies.in>
2022-09-29 12:24:56 +0530

    folder updated

---
+++ chambal/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+//const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const cors = require("cors");
+
+dotenv.config();
+
+// mongoose.connect(process.env.MONGO_URL)
+//     .then(() => console.log("DB Connection Successfull!"))
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/list', (req, res) => res.send("In hotel list"));
+
+app.get('/', (req, res) => res.send("In hotel info page"));
+
+
+app.listen(process.env.PORT || 5006, () => {
+    console.log("Hotel server is running on port! ", process.env.PORT);
+});
