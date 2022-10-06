85b6fe37e6f75ff165e4ed028660638536cd4d89
arsh <mmustaqueem@n2rtechnologies.in>
2022-10-06 11:34:35 +0530

    amenities update

--- client/frontend/src/components/Admin/Hotel/AmenityStatus.js
+++ client/frontend/src/components/Admin/Hotel/AmenityStatus.js
@@ -1,30 +1,72 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Select from 'react-select';
 
-const getAmenities = [
-  { value: 'Wifi', label: 'Wifi' },
-  { value: 'Swimming Pool', label: 'Swimming Pool' },
-  { value: 'In-House Restaurant', label: 'In-House Restaurant' },
-  { value: 'Hair Dryer', label: 'Hair Dryer' },
-];
+export default function AmenityStatus({ amenities }) {
+  const [selectedAmenities, setSelectedAmenities] = useState([]);
+  const getAmenities = [];
 
-export default function AmenityStatus() {
-  const [setSelectedAmenities] = useState(null);
+  const getSelectedData = async () => {
+
+    amenities.map((item) => {
+
+      if (item.status) {
+        selectedAmenities.push({
+          value: item.amenity,
+          label: item.amenity,
+        });
+      }
+
+    });
+  }
+
+  const getAllAmenities = async () => {
+
+    await amenities.map((item) => {
+
+      getAmenities.push({
+        value: item.amenity,
+        label: item.amenity,
+      });
+
+    });
+  }
+
+
+  const selectAmenities = () => {
+
+  };
+
+
+  useEffect(() => {
+
+    getAllAmenities();
+    if (amenities.length) {
+      getSelectedData();
+
+    }
+
+    console.log("call func ", amenities)
+
+  });
+
+
+
+
   return (
     <>
-    <div>AmenityStatus</div>
-    <div className='flex'>
-      <div>
-      <Select
-      defaultValue={[getAmenities[2], getAmenities[3]]}
-      onChange={setSelectedAmenities}
-      options={getAmenities}
-      isMulti
-      className='setReactSelect'
-      />
+      <div>AmenityStatus</div>
+      <div className='flex'>
+        <div>
+          <Select
+            defaultValue={selectedAmenities}
+            onChange={selectAmenities}
+            options={getAmenities}
+            isMulti
+            className='setReactSelect'
+          />
+        </div>
+        <div><button type="submit" className="text-white float-right bg-danger font-medium rounded px-5 py-2.5 text-center">Update</button></div>
       </div>
-      <div><button type="submit" className="text-white float-right bg-danger font-medium rounded px-5 py-2.5 text-center">Update</button></div>
-    </div>
     </>
   )
 }
