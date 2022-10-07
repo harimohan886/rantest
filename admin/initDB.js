3022b2c326a3ec0939a96e3c36f8e28c06e0e4b9
Nurul Hasan <nhasan@n2rtechnologies.com>
2022-10-07 10:47:48 +0530

    Test

--- admin/initDB.js
+++ admin/initDB.js
@@ -4,8 +4,6 @@ module.exports = () => {
   mongoose
     .connect(process.env.MONGO_URL, {
       dbName: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      pass: process.env.DB_PASS,
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
