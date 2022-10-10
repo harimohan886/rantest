807caaac8ee5564a54412bdf7864c9b063149b34
Nurul Hasan <nhasan@n2rtechnologies.com>
2022-10-10 12:31:37 +0530

    Package Fix

--- package/initDB.js
+++ package/initDB.js
@@ -3,9 +3,6 @@ const mongoose = require('mongoose');
 module.exports = () => {
   mongoose
     .connect(process.env.MONGO_URL, {
-      dbName: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      pass: process.env.DB_PASS,
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
