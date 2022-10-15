a8b087c95f444d786407f280f0a4c7908b3b6de7
Nurul Hasan <nhasan@n2rtechnologies.com>
2022-10-15 13:55:44 +0530

    Added Test

--- admin/test.js
+++ admin/test.js
@@ -3,7 +3,7 @@ var request = require('request');
 
 it('Main page content', function(done) {
     request('http://localhost:5005' , function(error, response, body) {
-        expect(body).to.equal('Test Ok');
+        expect(response.statusCode).to.equal(200);
         done();
     });
 });
