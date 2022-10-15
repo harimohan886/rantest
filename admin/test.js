18e691326b6e841c5277bd98c7f970d54e931172
Nurul Hasan <nhasan@n2rtechnologies.com>
2022-10-16 00:55:41 +0530

    Adding Unit Test

--- admin/test.js
+++ admin/test.js
@@ -2,7 +2,7 @@ var expect  = require('chai').expect;
 var request = require('request');
 
 it('Main page content', function(done) {
-    request('http://localhost:5005' , function(error, response, body) {
+    request('http://arsh/ranthambore/admin:5005' , function(error, response, body) {
         expect(response.statusCode).to.equal(200);
         done();
     });
