8bba5edff4cec34950cd46cf5ba1e4aebf9b2577
Krishna Mishra <mkrishna@n2rtechnologies.in>
2022-10-18 10:32:34 +0530

    changed

--- admin/Controllers/Customer.Controller.js
+++ admin/Controllers/Customer.Controller.js
@@ -2,7 +2,7 @@ const createError = require('http-errors');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const validator = require('../helpers/validate');
-const ApiFeatures = require("../utils/apifeatures");
+const ApiFeatures = require("../Utils/apifeatures");
 
 const asyncHandler = require('../Middleware/asyncHandler')
 
