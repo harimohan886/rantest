795418746e64c80dadbb078048fa736799eed4fb
arsh <mmustaqueem@n2rtechnologies.in>
2022-10-28 18:41:20 +0530

    category hotel section

--- client/frontend/src/components/frontend/Package/RoomType.js
+++ client/frontend/src/components/frontend/Package/RoomType.js
@@ -1,37 +1,51 @@
 import React from 'react'
 import ContentImage from './ContentImage'
 
-export default function RoomType() {
-  return (
-    <>
-    <ul className="nav nav-tabs" id="roomTab" role="tablist">
-        <li className="nav-item">
-            <a className="nav-link active" data-toggle="tab" href="#tab-standard" role="tab" aria-controls="tab-standard" aria-selected="true">Standard</a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link" data-toggle="tab" href="#tab-deluxe" role="tab" aria-controls="tab-deluxe" aria-selected="false">Deluxe</a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link" data-toggle="tab" href="#tab-premium" role="tab" aria-controls="tab-premium" aria-selected="false">Premium</a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link" data-toggle="tab" href="#tab-luxury" role="tab" aria-controls="tab-luxury" aria-selected="false">Luxury</a>
-        </li>
-    </ul>
-    <div className="tab-content" id="roomTabContent">
-        <div className="tab-pane active" id='tab-standard'>
-            <ContentImage/>
-        </div>
-        <div className="tab-pane fade" id='tab-deluxe'>
-            <ContentImage/>
-        </div>
-        <div className="tab-pane fade" id='tab-premium'>
-            <ContentImage/>
-        </div>
-        <div className="tab-pane fade" id='tab-luxury'>
-            <ContentImage/>
-        </div>
-    </div>
-    </>
-  )
+export default function RoomType({ packages }) {
+    return (
+        <>
+            <ul className="nav nav-tabs" id="roomTab" role="tablist">
+
+
+                {packages?.categories?.map((list, lindex) => (
+                    <li key={lindex} className="nav-item">
+                        <a className={`nav-link ${lindex === 0 ? 'active' : ''}`} data-toggle="tab" href={`#tab-cat${lindex}`} role="tab" aria-controls="tab-standard" aria-selected="true">{list.category}</a>
+                    </li>
+
+                ))}
+
+
+
+                {/* <li className="nav-item">
+                    <a className="nav-link" data-toggle="tab" href="#tab-deluxe" role="tab" aria-controls="tab-deluxe" aria-selected="false">Deluxe</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" data-toggle="tab" href="#tab-premium" role="tab" aria-controls="tab-premium" aria-selected="false">Premium</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" data-toggle="tab" href="#tab-luxury" role="tab" aria-controls="tab-luxury" aria-selected="false">Luxury</a>
+                </li> */}
+            </ul>
+            <div className="tab-content" id="roomTabContent">
+
+                {packages?.categories?.map((list, lindex) => (
+                    <div key={lindex} className={`tab-pane ${lindex === 0 ? 'active' : ''}`} id={`tab-cat${lindex}`}>
+                        <ContentImage hotels={list.hotels} counterkey={lindex} />
+                    </div>
+
+                ))}
+
+
+                {/* <div className="tab-pane fade" id='tab-deluxe'>
+                    <ContentImage />
+                </div>
+                <div className="tab-pane fade" id='tab-premium'>
+                    <ContentImage />
+                </div>
+                <div className="tab-pane fade" id='tab-luxury'>
+                    <ContentImage />
+                </div> */}
+            </div>
+        </>
+    )
 }
