f64fb080079ad0290964a85713bed77896c45547
ganesh <nganesh@n2rtechnologies.in>
2022-11-03 17:15:22 +0530

    customer filter fixed

--- client/frontend/src/pages/Admin/Customers/Customers.js
+++ client/frontend/src/pages/Admin/Customers/Customers.js
@@ -138,7 +138,7 @@ export default function Customers() {
                         <option value="chambal">Chambal</option>
                         <option value="package">Package</option>
                     </select>
-                </div>result_ustomers
+                </div>
                 <div className='form-group'>
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Customer Name</label>
                     <select id="safariType" value = {filterName} onChange = {(e) => setFilterName(e.target.value)} className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
