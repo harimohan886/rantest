a95c9842642da621b59cfb547225493078770063
ganesh <nganesh@n2rtechnologies.in>
2022-11-10 12:56:46 +0530

    readonly add in name

--- client/frontend/src/pages/Admin/SafariDates/EditZone.js
+++ client/frontend/src/pages/Admin/SafariDates/EditZone.js
@@ -66,7 +66,7 @@ export default function EditZone() {
             <form>
                 <div className='form-group'>
                     <label className='block mb-2 text-sm font-bold text-gray-900 dark:text-gray-300'>Zone name</label>
-                    <input type='text' value = {name} readOnly={true} onChange={(e) => setName(e.target.value)} className='bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' placeholder='Zone name'/>
+                    <input type='text' value = {name} onChange={(e) => setName(e.target.value)} className='bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' placeholder='Zone name'/>
                 </div>
                 <div className='form-group'>
                     <label className='block mb-2 text-sm font-bold text-gray-900 dark:text-gray-300'>Availability</label>
