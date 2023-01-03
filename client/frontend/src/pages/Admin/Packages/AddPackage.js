c2e85e172306328827bd3cd418e19f9998f4bdd0
Sohrab <website.design1008@gmail.com>
2023-01-03 19:08:52 +0530

    Update AddPackage.js

--- client/frontend/src/pages/Admin/Packages/AddPackage.js
+++ client/frontend/src/pages/Admin/Packages/AddPackage.js
@@ -113,7 +113,7 @@ export default function AddPackage() {
                         </div>
                         <div className='mb-3 mt-2'>
                             <label className="block mb-2 text-sm font-bold text-gray-900 dark:text-gray-300">About Package</label>
-                            <textarea name="description" onChange={handleChange} id="aboutPackage" rows="3" className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="About Hotel"></textarea>
+                            <textarea name="description" onChange={handleChange} id="aboutPackage" rows="3" className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="About Package"></textarea>
                         </div>
                         <div className='mb-3'>
                             <label className="block mb-2 text-sm font-bold text-gray-900 dark:text-gray-300">Meta title</label>
