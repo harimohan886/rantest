a6f854913d36a9db88eb2af4916725aac732fffc
fateh <gfateh@n2rtechnologies.in>
2023-01-09 20:49:30 +0530

    fix titles

--- client/frontend/src/components/frontend/Home2/HolidayTour.js
+++ client/frontend/src/components/frontend/Home2/HolidayTour.js
@@ -2,38 +2,49 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function HolidayTour({ packages }) {
+    const data = [];
+    packages && packages.map((itm, i) => {
+        if (i <= 4 && itm.homepage == 1) {
+            data.push(itm);
+        }
+    });
 
     return (
         <div className='container'>
             <div className='holidays'>
-                <h3 className='common-title'>Book your Holiday Tour Packages</h3>
+                {data && data.map((d, i) => {
+                    if (i == 0) {
+                        return <h3 className='common-title'>Book your Holiday Tour Packages</h3>
+                    }
+                })}
+
                 <div className='row'>
-                    {packages && packages.map((item, i) => {
-                        if (i <= 4 && item.homepage == 1) {
-                            return (<div className='col-sm-3'>
-                                <div className='paBox'>
-                                    <Link to={`/package-details/${item.slug}`}>
-                                        <img src={`${process.env.REACT_APP_PACKAGE_SERVER_URL}/` + item.image} className='img-fluid' alt='Holiday Package' />
-                                        <h4>{item.name}</h4>
-                                        <p>
-                                            {item.inclusions && item.inclusions.map((inc, i) => {
-                    
-                                                return inc.inclusion + ' ,';
-                                            })}
-                                        </p>
-                                        <h6>₹ {item.price}</h6>
-                                        <div className='star-buttons'>
-                                            <div className='star'>
-                                                <label>{item.rating}.0 <img src='../image/icons/star2.png' className='img-fluid' alt='Rating' /></label>
-                                            </div>
-                                            <div className='bookButton'>
-                                                <button className='btn btn-transparent'>Book Now</button>
-                                            </div>
+                    {data && data.map((item, i) => {
+
+                        return (<div className='col-sm-3'>
+                            <div className='paBox'>
+                                <Link to={`/package-details/${item.slug}`}>
+                                    <img src={`${process.env.REACT_APP_PACKAGE_SERVER_URL}/` + item.image} className='img-fluid' alt='Holiday Package' />
+                                    <h4>{item.name}</h4>
+                                    <p>
+                                        {item.inclusions && item.inclusions.map((inc, i) => {
+
+                                            return inc.inclusion + ' ,';
+                                        })}
+                                    </p>
+                                    <h6>₹ {item.price}</h6>
+                                    <div className='star-buttons'>
+                                        <div className='star'>
+                                            <label>{item.rating}.0 <img src='../image/icons/star2.png' className='img-fluid' alt='Rating' /></label>
                                         </div>
-                                    </Link>
-                                </div>
-                            </div>)
-                        }
+                                        <div className='bookButton'>
+                                            <button className='btn btn-transparent'>Book Now</button>
+                                        </div>
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>)
+
                     })}
                 </div>
             </div>
--- client/frontend/src/components/frontend/Home2/HotelsIn.js
+++ client/frontend/src/components/frontend/Home2/HotelsIn.js
@@ -1,37 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function HotelsIn({ hotels }) {
-  
+  const data = [];
+  hotels && hotels.map((hotel, i) => {
+    if (i <= 4 && hotel.homepage == 1) {
+      data.push(hotel);
+    }
+  });
+
   return (
     <div className='hotelbgIn'>
       <div className='container'>
-        <h3 className='common-title'>Hotels & Resorts in Ranthambore</h3>
+        {data && data.map((d, i) => {
+          if (i == 0) {
+            return <h3 className='common-title'>Hotels & Resorts in Ranthambore</h3>
+          }
+        })}
         <div className='row'>
-          {hotels && hotels.map((hotel, i) => {
-            if (i <= 4 && hotel.homepage == 1) {
-            
-              return (<div className='col-sm-3'>
-                <div className='hotelBox'>
-                  <Link to={ '/hotel-details/'+hotel.slug }>
-                    <img src={ `${process.env.REACT_APP_HOTEL_SERVER_URL}/`+hotel.image } className='img-fluid' alt='Hotel' />
-                    <h4>{ hotel.name }</h4>
-                    <ul className='list-group'>
-                      <li>{ hotel.city }, { hotel.state }</li>
-                      <li>Starting from <span>₹ { hotel.price }</span></li>
-                    </ul>
-                    <div className='star-buttons'>
-                      <div className='star'>
-                        <label>{ hotel.rating }.0 <img src='../image/icons/star2.png' className='img-fluid' alt='Rating' /></label>
-                      </div>
-                      <div className='bookButton'>
-                        <button className='btn btn-transparent'>Book Now</button>
-                      </div>
+          {data && data.map((hotel, i) => {
+
+            return (<div className='col-sm-3'>
+              <div className='hotelBox'>
+                <Link to={'/hotel-details/' + hotel.slug}>
+                  <img src={`${process.env.REACT_APP_HOTEL_SERVER_URL}/` + hotel.image} className='img-fluid' alt='Hotel' />
+                  <h4>{hotel.name}</h4>
+                  <ul className='list-group'>
+                    <li>{hotel.city}, {hotel.state}</li>
+                    <li>Starting from <span>₹ {hotel.price}</span></li>
+                  </ul>
+                  <div className='star-buttons'>
+                    <div className='star'>
+                      <label>{hotel.rating}.0 <img src='../image/icons/star2.png' className='img-fluid' alt='Rating' /></label>
                     </div>
-                  </Link>
-                </div>
-              </div>)
-            }
+                    <div className='bookButton'>
+                      <button className='btn btn-transparent'>Book Now</button>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            </div>)
+
           })}
         </div>
       </div>
