5af18583e41b3cc6ae0cc5eaae018c3afaed2fa4
Nurul Hasan <nhasan@n2rtechnologies.com>
2023-01-15 01:00:36 +0530

    Updated file Limit

--- package/index.js
+++ package/index.js
@@ -6,11 +6,12 @@ require('./initDB')();
 const cors = require("cors");
 
 const path = require('path')
-app.use(express.limit('100M'));
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 var bodyParser = require('body-parser');
 
+app.use(express.limit('100M'));
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
