1f01020c3f949312d27596b93cf906b2344d45c3
Krishna Mishra <mkrishna@n2rtechnologies.in>
2023-04-21 11:14:28 +0530

    chambal booking enquery

--- client/frontend/src/pages/frontend/ChambalBooking.js
+++ client/frontend/src/pages/frontend/ChambalBooking.js
@@ -2,6 +2,7 @@ import React , { useState , useEffect } from 'react'
 import { Link } from 'react-router-dom' 
 import { useAlert } from "react-alert"
 import axios from 'axios'
+import swal from 'sweetalert'
 import { useParams } from "react-router-dom";
 import moment from 'moment';
 import DatePicker from "react-datepicker"
@@ -23,6 +24,28 @@ export default function ChambalBooking() {
 
     const checkboxHandler = () => {
         setAgree(!agree);
+    }
+
+
+    const handleChangeMobile = (e) => {
+
+        setPhone(e.target.value);
+
+        const data = {
+            "name": name,
+            "mobile": e.target.value
+        }
+
+        if (e.target.value.length >= 10) {
+
+            axios.post(`${process.env.REACT_APP_BASE_URL}/admin/enquiries/save-enquery`, data).then(res => {
+                if (res.status === 200) {
+                } else {
+                }
+            }).catch(error => {
+                swal("Warning", error, "warning");
+            })
+        }
     }
 
     const IndianPersonHandle = (e) => {
@@ -239,7 +262,7 @@ export default function ChambalBooking() {
                     <div className='col-sm-6'>
                         <div className='form-group'>
                             <label>Mobile number</label>
-                            <input type='tel' className='form-control' onChange = {(e) => setPhone(e.target.value)}  placeholder='Mobile number' />
+                            <input type='tel' className='form-control' onChange={handleChangeMobile}  placeholder='Mobile number' />
                         </div>
                     </div>
 
