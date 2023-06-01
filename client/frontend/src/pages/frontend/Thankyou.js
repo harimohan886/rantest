6fa8cf5be6e5b4eb7e67b55ca211b716524c3fa2
Krishna Mishra <mkrishna@n2rtechnologies.in>
2023-06-01 17:59:22 +0530

    chnages

--- client/frontend/src/pages/frontend/Thankyou.js
+++ client/frontend/src/pages/frontend/Thankyou.js
@@ -13,6 +13,7 @@ export default function Thankyou() {
     <div className="container" style={{minHeight: "600px"}}>
         <div className="thankyoupage">
             <h1>Thank you for Booking with us, we will send you booking vouchers at your email id shortly.</h1>
+            <h4>The Payment does not guarantee Booking. Confirmed Booking would be available only as per seat availability</h4>
             <h3>For any query please contact + 91 7838 4986 45</h3>
             <div>&nbsp;</div>
             <div className="text-center">
